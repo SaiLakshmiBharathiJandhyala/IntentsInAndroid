80ef7e5247450bfb9605cce7ac26b35be6568c7d
SaiLakshmiBharathiJandhyala <bharathijandhyala1974@gmail.com>
2019-03-15 19:24:37 +0530

    App to show the usage of Explicit Intents
    Signed-off-by: SaiLakshmiBharathiJandhyala <bharathijandhyala1974@gmail.com>

--- app/src/main/java/com/example/bharathi/jandhyala/myapplication/MainActivity.java
+++ app/src/main/java/com/example/bharathi/jandhyala/myapplication/MainActivity.java
@@ -2,11 +2,23 @@ package com.example.bharathi.jandhyala.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+    CheckBox surfBox;
+    CheckBox readBox;
+    CheckBox playBox;
+    CheckBox tvBox;
+    CheckBox codeBox;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        surfBox = findViewById(R.id.surfHobby);
+        readBox = findViewById(R.id.readHobby);
+        playBox = findViewById(R.id.playHobby);
+        tvBox = findViewById(R.id.watchHobby);
+        codeBox = findViewById(R.id.practiceHobby);
     }
 }
