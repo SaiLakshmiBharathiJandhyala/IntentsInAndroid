0ef82b886fefc19aa8c300181bd0e3ac8b2f111e
SaiLakshmiBharathiJandhyala <bharathijandhyala1974@gmail.com>
2019-03-15 21:20:06 +0530

    App to show the usage of Explicit Intents
    Signed-off-by: SaiLakshmiBharathiJandhyala <bharathijandhyala1974@gmail.com>

--- app/src/main/java/com/example/bharathi/jandhyala/myapplication/DetailActivity.java
+++ app/src/main/java/com/example/bharathi/jandhyala/myapplication/DetailActivity.java
@@ -2,9 +2,25 @@ package com.example.bharathi.jandhyala.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class DetailActivity extends AppCompatActivity {
-
+    @BindView(R.id.feedbacknote)
+    TextView textView;
+    @BindView(R.id.feedbacknote)
+    TextView textView1;
+    @BindView(R.id.feedbacknote)
+    TextView textView2;
+    @BindView(R.id.feedbacknote)
+    TextView textView3;
+    @BindView(R.id.feedbacknote)
+    TextView textView4;
+    @BindView(R.id.feedbacknote)
+    TextView textView5;
+    @BindView(R.id.feedbacknote)
+    TextView textView6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
